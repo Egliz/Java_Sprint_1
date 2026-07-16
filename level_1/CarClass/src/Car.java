b584495491e32417f490733aaa310b8bf00c7b37
Egliz <egliz3010@gmail.com>
2026-07-16 11:04:17 +0200

    feat: create brake method (CarClass)

--- level_1/CarClass/src/Car.java
+++ level_1/CarClass/src/Car.java
@@ -1,5 +1,16 @@
 public class Car {
 
+    static final String brand;
+    static String model;
+    final boolean power;
+
+    public car () {
+
+    }
+
+    static void brake() {
+        System.out.println("The vehicle is braking");
+    }
 
     }
 }
