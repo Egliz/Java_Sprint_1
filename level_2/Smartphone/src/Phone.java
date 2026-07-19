c77b643b7a95485234fb92e8c065c1a523d1e238
Egliz <egliz3010@gmail.com>
2026-07-19 10:43:49 +0200

    feat: add call method

--- level_2/Smartphone/src/Phone.java
+++ level_2/Smartphone/src/Phone.java
@@ -2,4 +2,15 @@ public class Phone {
 
     private String brand;
     private String model;
+
+    public Phone (String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+
+    }
+
+    //implementar verificacion que sea un numero
+    public void call (String phoneNumber) {
+        System.out.println("Calling " + phoneNumber + "...");
+    }
 }
