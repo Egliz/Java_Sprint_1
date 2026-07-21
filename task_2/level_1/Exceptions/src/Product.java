ef0e795d4f527cd69a3bd65a51a9b2d1cd5309ee
Egliz <egliz3010@gmail.com>
2026-07-21 13:03:44 +0200

    feat: add Product class

---
+++ task_2/level_1/Exceptions/src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+
+    private String name;
+    private int price;
+
+    public product(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+}
