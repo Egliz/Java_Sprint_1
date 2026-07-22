56d1142bae3616ea88e6976e512125538b1d992b
Egliz <egliz3010@gmail.com>
2026-07-22 14:23:10 +0200

    feat: change EmptySaleException to extend from RuntimeException

--- task_2/level_1/Exceptions/src/EmptySaleException.java
+++ task_2/level_1/Exceptions/src/EmptySaleException.java
@@ -1,4 +1,4 @@
-public class EmptySaleException extends Exception{
+public class EmptySaleException extends RuntimeException{
     public EmptySaleException(String message) {
         super(message);
     }
