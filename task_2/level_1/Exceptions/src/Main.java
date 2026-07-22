9eb56324024b94889ee2fd94ffaceffffc8de1eb
Egliz <egliz3010@gmail.com>
2026-07-22 13:53:39 +0200

    test: add IndexOutOfBoundsExceptionn test

--- task_2/level_1/Exceptions/src/Main.java
+++ task_2/level_1/Exceptions/src/Main.java
@@ -4,9 +4,25 @@ public class Main {
 
         Sale sale1 = new Sale();
         Product product1 = new Product("PC", 7);
+        Product product2 = new Product("Keyboard", 10);
 
         sale1.addProduct(product1);
+        sale1.addProduct(product2);
         sale1.calculateTotal();
         System.out.println(sale1.calculateTotal());
+
+        try {
+            sale1.products.get(2);
+        } catch (Exception IndexOutOfBoundsExceptionn ) {
+            System.out.println("Exception in thread \"main\" java.lang.IndexOutOfBoundsException: Index 2 out of bounds for length 2\n" +
+                    "\tat java.base/jdk.internal.util.Preconditions.outOfBounds(Preconditions.java:100)\n" +
+                    "\tat java.base/jdk.internal.util.Preconditions.outOfBoundsCheckIndex(Preconditions.java:106)\n" +
+                    "\tat java.base/jdk.internal.util.Preconditions.checkIndex(Preconditions.java:302)");
+        }
+
+
+
+
+
     }
 }
--- task_2/level_1/Exceptions/src/Sale.java
+++ task_2/level_1/Exceptions/src/Sale.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
     public class Sale {
 
         private double totalPrice;
-        private ArrayList<Product> products;
+        public ArrayList<Product> products;
 
         public Sale() {
             products = new ArrayList<Product>();
